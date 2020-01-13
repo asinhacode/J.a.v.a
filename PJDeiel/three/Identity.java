84c84d06c090728fa77a7cb1f4073f774e37a867
Anupam Sinha <57113053+asinhacode@users.noreply.github.com>
2020-01-13 12:37:50 -0500

    Create Identity.java

---
+++ PJDeiel/three/Identity.java
@@ -0,0 +1,21 @@
+package three;
+
+public class Identity 
+{
+	private String name;
+	
+		public Identity(String name)
+		{
+			this.name = name;
+		}
+		
+		public String getName()
+		{
+			return name;
+		}
+		
+		public void setName(String name)
+		{
+			this.name = name;
+		}
+}
